e0465710d3a428199818f18e924c4c09f14337a4
Ville Laitila <ville.laitila@softagram.com>
2021-08-12 22:44:00 +0300

    improve documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # allrepos.py: get stuff done with 100x git repo directories
-Allrepos python utility for running operations with multiple repos
+
+Utility for working with multiple repos when repos have been placed in a single top level dir
+
+# Couple of example use cases
+
+## Run fetch for all
+`python allrepos.py fetch`
+
+## Get default branches
+`python allrepos.py symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@ @'`
+
+## Delete all branches with name foo from remotes:
+`python allrepos.py ls-remote --exit-code --heads origin foo IF_OK_EXECUTE_ALSO push origin --delete foo`
+
+
+
+# Requirements
+- Python 3.6+ since uses f-strings.
+- Git.
+
+Note: the examples here involve usage of unix tools, but this is not limited to unix.
--- allrepos.py
+++ allrepos.py
@@ -1,22 +1,14 @@
-# Utility for working with multiple repos when repos have been placed in a single top level dir
-#
-# Example use cases:
-#
-# Get default branches.
-#  python allrepos.py symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@ @'
-#
-# Delete all branches with name foo from remotes:
-#  python allrepos.py ls-remote --exit-code --heads origin foo IF_OK_EXECUTE_ALSO push origin --delete foo
+# See README.md for documentation.
 
-
-import sys, os, re
+import sys
+import os
 
 args = sys.argv[1:]
 ext_slot = ''
 ext_args = []
 if 'IF_OK_EXECUTE_ALSO' in args:
     pos = args.index('IF_OK_EXECUTE_ALSO')
-    ext_args = args[pos+1:]
+    ext_args = args[pos + 1:]
     args = args[:pos]
 
 for fn in os.listdir('.'):
@@ -25,4 +17,3 @@ for fn in os.listdir('.'):
         if ext_args:
             ext_slot = f' && git -C {fn} ' + ' '.join(ext_args)
         os.system(main_cmd + ext_slot)
-
